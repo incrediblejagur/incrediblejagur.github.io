f7313564a825dafe202f4893efe711b26e2d2cfb
Daniel <danielminter13@gmail.com>
2019-04-11 12:59:27 +0200

    a

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function mytabs(evt, names) {
+function pagetabs(Names) {
   var i, tabcontent, tablinks;
  
  
@@ -7,11 +7,6 @@ function mytabs(evt, names) {
     tabcontent[i].style.display = "none";
   }
   
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  
-  document.getElementById(names).style.display = "block";
+  document.getElementById(Names).style.display = "block";
   evt.currentTarget.className += " active";
 }
